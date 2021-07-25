bbcad99f913a120494c8b064b5b8e79d75ba8fda
AvianJay <48914361+AvianJay@users.noreply.github.com>
2021-07-25 21:27:26 +0800

    add nul

--- internetchecker.bat
+++ internetchecker.bat
@@ -5,7 +5,7 @@ echo Press Any Key To Check Internet.
 :a
 pause > nul
 echo Checking...
-curl 1.1.1.1 --output netcheck.tmp
+curl 1.1.1.1 --output netcheck.tmp > nul
 if exist "netcheck.tmp" (echo You Connected internet. & del netcheck.tmp) else (echo You are not connected to the internet.)
 echo Press Any Key To Check Internet Again.
 goto a
