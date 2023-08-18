d18a35647d7a29e5a071d4341a927af761402ecf
AvianJay <48914361+AvianJay@users.noreply.github.com>
2023-08-18 14:48:03 +0800

    ah not x86

--- avianchat.bat
+++ avianchat.bat
@@ -2,7 +2,7 @@
 
 :check
 echo Checking...
-IF EXIST "C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe" (
+IF EXIST "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe" (
 	goto :connect
     ) ELSE (
 	echo You don't have Chrome installed, press any button to recheck.
@@ -12,10 +12,10 @@ IF EXIST "C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe" (
 :connect
 echo Connecting...
 cd %temp%
-curl http://www.ajmcserver.ml/text.txt --output test.txt > nul
+curl https://avianjay.eu.org/text.txt --output test.txt > nul
 IF EXIST test.txt (
 	del test.txt
-	"C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe" --app=http://www.ajmcserver.ml/chat.html
+	"C:\Program Files\Google\Chrome\Application\chrome_proxy.exe" --app=http://avianjay.eu.org/chat.html
 	exit
     ) ELSE (
 	echo Can't Connect to Server.
