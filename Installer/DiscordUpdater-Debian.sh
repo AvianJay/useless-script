e4162a06b5abe9ef0a0e12cd74988ac9653813e5
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-08-10 19:32:33 +0800

    debian discord updater
    so u wont go to discord download page again

---
+++ Installer/DiscordUpdater-Debian.sh
@@ -0,0 +1,17 @@
+if [ -e /tmp/Discord.deb ]
+then
+sudo rm /tmp/Discord.deb
+fi
+wget "https://discord.com/api/download?platform=linux&format=deb" -O /tmp/Discord.deb
+sudo dpkg -i /tmp/Discord.deb
+set returncode = $?
+if [ $returncode == 0 ]
+then
+screen -L -dmS Discord /bin/bash -c discord
+else
+echo ERROR! Cannot install Discord!
+echo Press any key to continue...
+read
+exit 1
+fi
+exit
