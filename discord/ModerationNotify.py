9ae7d0ec77d0f70b585c465e2a14c070969de49e
AvianJay <jay20090728@gmail.com>
2025-10-12 21:39:40 +0800

    fix ahhh

--- discord/ModerationNotify.py
+++ discord/ModerationNotify.py
@@ -87,7 +87,7 @@ async def on_member_update(before, after):
         # 檢查database的值避免重複
         if get_user_data(after.guild.id, after.id, "muted_until") == after.timed_out_until.isoformat():
             return
-        if after.communication_disabled_until <= datetime.now(timezone.utc):
+        if after.timed_out_until <= datetime.now(timezone.utc):
             return
         set_user_data(after.guild.id, after.id, "muted_until", after.timed_out_until.isoformat())
         guild = after.guild
