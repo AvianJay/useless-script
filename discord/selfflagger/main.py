10d49ad2d729c83524adb4f70538ef44df045884
AvianJay <jay20090728@gmail.com>
2025-10-22 07:43:40 +0800

    ahhhh fix

--- discord/selfflagger/main.py
+++ discord/selfflagger/main.py
@@ -238,6 +238,6 @@ async def on_ready():
         print('[+] Starting flagged users update...')
         tta, ttaf = await update_flagged_users()
         print(f'[+] Flagged users update complete. Added: {tta}, +Flagged: {ttaf}')
-        await asyncio.wait(300)
+        await asyncio.sleep(300)
 
 bot.run(config("token"))
