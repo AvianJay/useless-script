522852cd6aef8ca478584681fc019bd31e9c6393
AvianJay <jay20090728@gmail.com>
2025-12-31 01:30:31 +0800

    0.16.3

--- discord/UtilCommands.py
+++ discord/UtilCommands.py
@@ -9,7 +9,7 @@ from datetime import datetime, timezone
 import psutil
 
 startup_time = datetime.now(timezone.utc)
-version = "0.16.2"
+version = "0.16.3"
 try:
     git_commit_hash = os.popen("git rev-parse --short HEAD").read().strip()
 except Exception as e:
