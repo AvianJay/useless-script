2cf10c8663afa6736b34ecf57567548856846ce2
AvianJay <jay20090728@gmail.com>
2026-02-10 22:21:52 +0800

    0.18.3

--- discord/UtilCommands.py
+++ discord/UtilCommands.py
@@ -12,7 +12,7 @@ import aiohttp
 from database import db
 
 startup_time = datetime.now(timezone.utc)
-version = "0.18.2"
+version = "0.18.3"
 try:
     git_commit_hash = os.popen("git rev-parse --short HEAD").read().strip()
 except Exception as e:
