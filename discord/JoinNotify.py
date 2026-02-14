1db027b18a8394a845be591ae80854dfb676ff06
AvianJay <jay20090728@gmail.com>
2026-02-14 16:35:38 +0800

    fix

--- discord/JoinNotify.py
+++ discord/JoinNotify.py
@@ -13,8 +13,10 @@ class JoinNotify(commands.Cog):
         app_commands.Choice(name="好啊", value="enable"),
         app_commands.Choice(name="不要", value="disable")
     ])
+    @app_commands.allowed_contexts(guilds=True, dm=True, private_channels=True)
+    @app_commands.allowed_installs(guilds=True, users=True)
     async def joinnotify(self, interaction: discord.Interaction, option: str):
-        if option.value == "enable":
+        if option == "enable":
             set_user_data(0, interaction.user.id, "join_notify", True)
             await interaction.response.send_message("已啟用加入通知！當你邀請我加入伺服器時，我會私訊你一個歡迎訊息！", ephemeral=True)
         else:
@@ -30,12 +32,12 @@ class JoinNotify(commands.Cog):
                 inviter = entry.user
                 break
         if inviter:
-            if not get_user_data(0, inviter.id, "join_notify", False):
+            if not get_user_data(0, inviter.id, "join_notify", True):
                 return
             try:
                 embed = discord.Embed(
                     title="欸？你好像邀請了我？",
-                    description=f"你好啊，{inviter.mention}！感謝你邀請我加入 {guild.name}！\n快速開始：\n- {get_command_mention('help')} 查看指令列表\n- {get_command_mention('tutorial')} 查看使用教學\n- {get_command_mention('panel')} 開啟網頁面板\n如果有任何問題，歡迎加入[支援伺服器]({config('support_server_invite')})尋求幫助！",
+                    description=f"你好啊，{inviter.mention}！感謝你邀請我加入 {guild.name}！\n快速開始：\n- {get_command_mention('help')} 查看指令列表\n- {get_command_mention('tutorial')} 查看使用教學\n- {get_command_mention('panel')} 開啟網頁面板\n如果有任何問題，歡迎加入[支援伺服器]({config('support_server_invite')})尋求幫助！\n\n-# 不想收到這些訊息？你可以使用 {get_command_mention('joinnotify')} 指令來關閉加入通知！",
                     color=discord.Color.green()
                 )
                 embed.set_footer(text=guild.name, icon_url=guild.icon.url if guild.icon else None)
@@ -50,12 +52,12 @@ class JoinNotify(commands.Cog):
         else:
             # dm the owner of the guild if we can't find the inviter
             owner = guild.owner
-            if not get_user_data(0, owner.id, "join_notify", False):
+            if not get_user_data(0, owner.id, "join_notify", True):
                 return
             try:
                 embed = discord.Embed(
                     title="欸？好像有人邀請了我？",
-                    description=f"你好啊，{owner.mention}！好像有人把我邀請進入你的伺服器 {guild.name} 了？但是我沒有權限可以知道他是誰 :/\n快速開始：\n- {get_command_mention('help')} 查看指令列表\n- {get_command_mention('tutorial')} 查看使用教學\n- {get_command_mention('panel')} 開啟網頁面板\n如果有任何問題，歡迎加入[支援伺服器]({config('support_server_invite')})尋求幫助！",
+                    description=f"你好啊，{owner.mention}！好像有人把我邀請進入你的伺服器 {guild.name} 了？但是我沒有權限可以知道他是誰 :/\n快速開始：\n- {get_command_mention('help')} 查看指令列表\n- {get_command_mention('tutorial')} 查看使用教學\n- {get_command_mention('panel')} 開啟網頁面板\n如果有任何問題，歡迎加入[支援伺服器]({config('support_server_invite')})尋求幫助！\n\n-# 不想收到這些訊息？你可以使用 {get_command_mention('joinnotify')} 指令來關閉加入通知！",
                     color=discord.Color.green()
                 )
                 embed.set_footer(text=guild.name, icon_url=guild.icon.url if guild.icon else None)
