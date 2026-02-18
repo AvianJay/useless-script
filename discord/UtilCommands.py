b54d25771c565ccefe92eb56bf0542f61c441005
AvianJay <jay20090728@gmail.com>
2026-02-18 18:17:35 +0800

    0.19.4

--- discord/UtilCommands.py
+++ discord/UtilCommands.py
@@ -14,7 +14,7 @@ from database import db
 from CustomPrefix import get_prefix
 
 startup_time = datetime.now(timezone.utc)
-version = "0.19.3"
+version = "0.19.4"
 try:
     git_commit_hash = os.popen("git rev-parse --short HEAD").read().strip()
 except Exception as e:
