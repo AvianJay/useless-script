66bb6816d83a1404448e4048e119279b8dd69a83
AvianJay <jay20090728@gmail.com>
2026-03-11 15:11:55 +0800

    sb copilot

--- discord/UtilCommands.py
+++ discord/UtilCommands.py
@@ -192,7 +192,7 @@ async def info(ctx: commands.Context, full: bool = False):
             embed.add_field(name=f"已禁用模組數量", value=str(len(config("disable_modules", []))), inline=False)
         if failed_modules:
             embed.add_field(name=f"載入失敗的模組數量", value=str(len(failed_modules)), inline=False)
-    embed.add_field(name="相關連結", value=f"* [機器人網站]({config('website_url')})\\n* [使用文檔]({config('website_url')}/docs)\\n* [支援伺服器]({config('support_server_invite')})\\n* [隱私政策]({config('website_url')}/privacy-policy)\\n* [服務條款]({config('website_url')}/terms-of-service)\\n* [邀請機器人](https://discord.com/oauth2/authorize?client_id={str(bot.user.id)})", inline=False)
+    embed.add_field(name="相關連結", value=f"* [機器人網站]({config('website_url')})\n* [使用文檔]({config('website_url')}/docs)\n* [支援伺服器]({config('support_server_invite')})\n* [隱私政策]({config('website_url')}/privacy-policy)\n* [服務條款]({config('website_url')}/terms-of-service)\n* [邀請機器人](https://discord.com/oauth2/authorize?client_id={str(bot.user.id)})", inline=False)
     embed.set_thumbnail(url=bot.user.avatar.url if bot.user.avatar else None)
     embed.timestamp = datetime.now(timezone.utc)
     embed.set_footer(text="by AvianJay")
